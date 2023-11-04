65a5ba3f5830b68e6732448dc64d4d9f1a3a74ed
Mauro Daniel Viveros <maurod.viveros@gmail.com>
2023-11-04 02:33:13 -0300

    feat: :sparkles: add routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from "@angular/router";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: "",
+    loadComponent: () => import("./modules/home/home.component").then(m => m.HomeComponent),
+    data: {
+      navigation: { icon: "home", text: "Inicio" },
+    },
+  },
+];
