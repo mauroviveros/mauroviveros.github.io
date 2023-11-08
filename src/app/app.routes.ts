d5293e7689525abb4c0f67e697bd9bcdb34a63bb
Mauro Daniel Viveros <maurod.viveros@gmail.com>
2023-11-08 14:57:14 -0300

    feat: :rotating_light: add eslint and fix linter

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,3 @@
 import { Routes } from "@angular/router";
 
-export const routes: Routes = [
-  {
-    path: "",
-    loadComponent: () => import("./modules/home/home.component").then(m => m.HomeComponent),
-    data: {
-      navigation: { icon: "home", text: "Inicio" },
-    },
-  },
-];
+export const routes: Routes = [];
