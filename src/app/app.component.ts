08875df638b6a0391b8beac3658611f9ee92a4e3
Mauro Daniel Viveros <maurod.viveros@gmail.com>
2023-11-08 14:53:07 -0300

    feat: :package: update angular 17

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,11 @@
 import { Component } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { RouterOutlet } from "@angular/router";
-import { CoreModule } from "./core/core.module";
 
 @Component({
   selector: "portfolio-root",
   standalone: true,
-  imports: [CommonModule, RouterOutlet, CoreModule],
+  imports: [CommonModule, RouterOutlet],
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.scss"],
 })
