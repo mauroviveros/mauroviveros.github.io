af871dd5f02782195085bdcfb8a04c30f8818d51
Mauro Daniel Viveros <maurod.viveros@gmail.com>
2023-11-08 23:37:03 -0300

    feat: :sparkles: add navList for routes

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,19 +1,25 @@
-import { Component } from "@angular/core";
+import { Component, inject } from "@angular/core";
 import { SharedModule } from "../../shared/shared.module";
 import { SocialComponent } from "../social/social.component";
 
 import { COPYRIGHT, USER } from "../../../environments/environment";
+import { Router, RouterModule } from "@angular/router";
 
 @Component({
   selector: "portfolio-navbar",
   standalone: true,
-  imports: [SharedModule, SocialComponent],
+  imports: [RouterModule, SharedModule, SocialComponent],
   templateUrl: "./navbar.component.html",
   styleUrl: "./navbar.component.scss",
 })
 export class NavbarComponent {
+  private readonly router = inject(Router);
   readonly fullname = [...USER.names, ...USER.surnames].join(" ");
   readonly role = USER.roles.join(" | ");
   readonly profileURL = USER.profileURL;
   readonly copyright = COPYRIGHT;
+  readonly routerLinkActiveOptions = { exact: true };
+  readonly menu = this.router.config.map(route => {
+    return Object.assign({ path: route.path }, route.data ? route.data["navigation"] : {});
+  });
 }
