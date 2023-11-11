22552afcb3210663b0b59d084b19064c62275cf6
Mauro Daniel Viveros <maurod.viveros@gmail.com>
2023-11-11 14:04:47 -0300

    feat(about): :truck: add routing for about

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,7 +5,14 @@ export const routes: Routes = [
     path: "",
     loadComponent: () => import("./modules/home/home.component").then(m => m.HomeComponent),
     data: {
-      navigation: { icon: "home", text: "Inicio" },
+      navigation: { icon: "home", text: "home" },
+    },
+  },
+  {
+    path: "about",
+    loadComponent: () => import("./modules/about/about.component").then(m => m.AboutComponent),
+    data: {
+      navigation: { icon: "badge", text: "about" },
     },
   },
 ];
