7e6efd9f9767bcbedcf5107bb529c8c00b698188
Mauro Daniel Viveros <maurod.viveros@gmail.com>
2024-02-05 13:08:58 -0300

    feat(experience): :sparkles: use jsonresume to get info for work experience

--- src/app/modules/experience/experience.component.ts
+++ src/app/modules/experience/experience.component.ts
@@ -1,4 +1,5 @@
 import { Component } from "@angular/core";
+import { CV } from "../../../environments/cv";
 
 @Component({
   selector: "portfolio-experience",
@@ -7,4 +8,6 @@ import { Component } from "@angular/core";
   templateUrl: "./experience.component.html",
   styleUrls: ["../../shared/scss/layout.scss", "./experience.component.scss"],
 })
-export class ExperienceComponent {}
+export class ExperienceComponent {
+  readonly works = CV.work;
+}
