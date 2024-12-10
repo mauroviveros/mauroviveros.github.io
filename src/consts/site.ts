4d60ac1c7efec091388d06898d8e705f36022b6c
Mauro Daniel viveros <maurod.viveros@gmail.com>
2024-12-10 17:18:57 -0300

    feat: :bento: add brand

--- src/consts/site.ts
+++ src/consts/site.ts
@@ -1,7 +1,7 @@
 import type { Site } from "@types";
 
 export const SITE: Site = {
-    TITLE: "Astro Sphere",
-    DESCRIPTION: "Welcome to Astro Sphere, a portfolio and blog for designers and developers.",
-    AUTHOR: "Mark Horn",
+    TITLE: "mviveros",
+    DESCRIPTION: "Welcome to my official page, a space to connect and learn more about me.",
+    AUTHOR: "Mauro Daniel Viveros",
   }
