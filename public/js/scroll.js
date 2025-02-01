2c5573db33ebe6ae575bf7e634171d455820ecc6
Mauro Daniel viveros <maurod.viveros@gmail.com>
2025-02-01 13:18:45 -0300

    chore: :coffin: remove console.log

--- public/js/scroll.js
+++ public/js/scroll.js
@@ -1,12 +1,6 @@
 function onScroll() {
   const header = document.getElementById("header");
-
-  console.log(window.scrollY);
-  if (window.scrollY > 0) {
-    header.classList.add("scrolled");
-  } else {
-    header.classList.remove("scrolled");
-  }
+  header.classList[window.scrollY ? "add" : "remove"]("scrolled");
 }
 
 document.addEventListener("scroll", onScroll);
