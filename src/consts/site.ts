62f0c3b8ef88c86e0b4f725fb2cb5cc43b9a030a
Mauro Daniel viveros <maurod.viveros@gmail.com>
2025-02-01 02:38:36 -0300

    feat: :globe_with_meridians: use i18n in head

--- src/consts/site.ts
+++ src/consts/site.ts
@@ -2,6 +2,6 @@ export const SITE = {
     TITLE: "mviveros",
     AUTHOR: "Mauro Daniel Viveros",
     ROLE: "DEV WEB | DEV OPS",
-    DESCRIPTION: "Welcome to my official page, a space to connect and learn more about me.",
+    DESCRIPTION: "site.description",
     KEYWORDS: "Mauro Daniel Viveros, Developer, Fullstack, Web, Mobile, DevOps, Programming, Technologies, Software, Development, Mauro Viveros"
   }
