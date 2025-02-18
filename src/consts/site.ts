af32375b46e68e98549fb0d7ad460712736dcb48
Mauro Daniel viveros <maurod.viveros@gmail.com>
2025-02-18 02:18:57 -0300

    feat: :sparkles: add favicon

--- src/consts/site.ts
+++ src/consts/site.ts
@@ -3,5 +3,6 @@ export const SITE = {
     AUTHOR: "Mauro Daniel Viveros",
     ROLE: "DEV WEB | DEV OPS",
     DESCRIPTION: "site.description",
-    KEYWORDS: "Mauro Daniel Viveros, Developer, Fullstack, Web, Mobile, DevOps, Programming, Technologies, Software, Development, Mauro Viveros"
+    KEYWORDS: "Mauro Daniel Viveros, Developer, Fullstack, Web, Mobile, DevOps, Programming, Technologies, Software, Development, Mauro Viveros",
+    FAVICON: "https://res.cloudinary.com/mviveros/image/upload/v1738690096/golang.png",
   }
