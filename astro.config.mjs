1db498b944796924bc9489a0d870359b80505392
Mauro Daniel viveros <maurod.viveros@gmail.com>
2025-02-22 13:05:02 -0300

    chore: ⬆️ upgrade tailwind 4

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,10 @@
 // @ts-check
+import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
-import { locales } from './src/i18n/index.ts';
-import icon from 'astro-icon';
-
-const defaultLocale = locales[0];
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), icon()],
-  i18n: {
-    locales,
-    defaultLocale
-  },
+    vite: {
+        plugins: [tailwindcss()],
+    },
 });
