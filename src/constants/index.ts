99eff1d96d6589e1ce78f24ed9d6c84ea5e00bd1
Mauro Daniel viveros <maurod.viveros@gmail.com>
2025-02-22 14:47:20 -0300

    feat: :sparkles: add Networks

---
+++ src/constants/index.ts
@@ -0,0 +1,4 @@
+import type { Network } from '@interfaces';
+import networks from './networks.json';
+
+export const NETWORKS = networks as Network[];
---
+++ src/interfaces/index.ts
@@ -0,0 +1,6 @@
+export interface Network {
+    network: string;
+    username: string;
+    url: string;
+    icon: string;
+}
