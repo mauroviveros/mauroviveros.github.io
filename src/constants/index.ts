4d45a434d6299cd7f483542ae436e4d8bd09579b
Mauro Daniel viveros <maurod.viveros@gmail.com>
2025-02-23 02:43:01 -0300

    feat: :egg: add useless sites

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,6 +1,8 @@
 import type { Feature, Network } from '@interfaces';
 import networks from './networks.json';
 import features from './features.json';
+import sites from './sites.json';
 
 export const NETWORKS = networks as Network[];
 export const FEATURES = features as Feature[];
+export const USELESS_SITES = sites as string[];
