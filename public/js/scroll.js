5201181fa55b5355b0217b96b69178bb4c2c97ca
Mauro Daniel viveros <maurod.viveros@gmail.com>
2025-02-23 00:08:11 -0300

    chore: :coffin: remove dead code

--- public/js/scroll.js
+++
@@ -1,6 +0,0 @@
-function onScroll() {
-  const header = document.getElementById("header");
-  header.classList[window.scrollY ? "add" : "remove"]("scrolled");
-}
-
-document.addEventListener("scroll", onScroll);
