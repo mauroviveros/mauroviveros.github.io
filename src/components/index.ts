9f647898d88c6af636276d2573b473a9abcc5bdf
Mauro Daniel viveros <maurod.viveros@gmail.com>
2025-09-10 15:52:38 -0300

    feat: :sparkles: add theme toggle functionality

--- src/components/index.ts
+++ src/components/index.ts
@@ -5,3 +5,4 @@ export { default as Availability } from "./Availability.astro";
 export { default as Connect } from "./Connect.astro";
 export { default as Experience } from "./Experience.astro";
 export { default as Projects } from "./Projects.astro";
+export { default as ThemeToggle } from "./ThemeToggle.astro";
