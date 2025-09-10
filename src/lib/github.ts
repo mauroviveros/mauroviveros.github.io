309008a2ae345f1f5e1ce7a6ed4a37c90954e5e3
Mauro Daniel viveros <maurod.viveros@gmail.com>
2025-09-10 00:57:26 -0300

    feat: :sparkles: update user information

--- src/lib/github.ts
+++ src/lib/github.ts
@@ -1,11 +1,16 @@
 import { GITHUB_TOKEN } from "astro:env/server";
 
 interface Repository {
-  name: string;
-  description: string;
-  topics: string[];
-  homepage: string;
-  html_url: string;
+  readonly name: string;
+  readonly description: string;
+  readonly topics: string[];
+  readonly homepage: string;
+  readonly html_url: string;
+}
+
+interface User {
+  readonly public_repos: number;
+  readonly created_at: string;
 }
 
 export const getRepos = async () => {
@@ -16,4 +21,14 @@ export const getRepos = async () => {
   });
 
   return (await response.json()) as Repository[];
+}
+
+export const getUser = async () => {
+  const response = await fetch('https://api.github.com/users/mauroviveros', {
+    headers: {
+      Authorization: `Bearer ${GITHUB_TOKEN}`,
+    },
+  });
+
+  return (await response.json()) as User;
 }
