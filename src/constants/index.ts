02c3bdb44d4c08498f30af87e7f1e93424a44454
Mauro Daniel viveros <maurod.viveros@gmail.com>
2025-09-10 16:26:31 -0300

    feat: :sparkles: refactor Experience

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,9 +1,17 @@
 import networks from './NETWORKS.json';
+import experience from './EXPERIENCE.json';
 
 interface Network {
-  name: string;
-  url: string;
-  icon: string;
+  readonly name: string;
+  readonly url: string;
+  readonly icon: string;
+}
+interface Experience {
+  readonly company: "Comunidad IT",
+  readonly role: "Programming Instructor",
+  readonly period: "2022 - 2023",
+  readonly initials: "IT"
 }
 
 export const NETWORKS = networks as Network[];
+export const EXPERIENCE = experience as Experience[];
--- src/lib/github.ts
+++ src/lib/github.ts
@@ -1,11 +1,15 @@
+import { site } from "astro:config/server";
 import { API_GITHUB_TOKEN } from "astro:env/server";
+const API_URL = 'https://api.github.com/users/mauroviveros';
 
 interface Repository {
   readonly name: string;
   readonly description: string;
   readonly topics: string[];
-  readonly homepage: string;
+  readonly homepage?: string;
   readonly html_url: string;
+  readonly private: boolean;
+  readonly fork: boolean;
 }
 
 interface User {
@@ -16,14 +20,23 @@ interface User {
 const request: RequestInit = { headers: { Authorization: `Bearer ${API_GITHUB_TOKEN}` } }
 
 export const getRepos = async () => {
-  const response = await fetch('https://api.github.com/users/mauroviveros/repos?sort=created', request);
+  const response = await fetch(`${API_URL}/repos?sort=updated`, request);
 
   const data = await response.json();
-  return (Array.isArray(data) ? data : []) as Repository[];
+  const repos = (Array.isArray(data) ? data : []) as Repository[];
+
+  return repos
+    .filter(repo => !repo.private)
+    .filter(({ fork }) => !fork)
+    .filter(({ topics }) => !topics.includes('github-profile'))
+    .filter(({ homepage }) => {
+      if(!homepage || !site) return true;
+      return new URL(homepage).href !== new URL(site).href;
+    });
 }
 
 export const getUser = async () => {
-  const response = await fetch('https://api.github.com/users/mauroviveros', request);
+  const response = await fetch(API_URL, request);
 
   return (await response.json()) as User;
 }
